47d1a5dad2439477032734a8e1e101fc0c9d9f00
vrtsev <v.rtsev@gmail.com>
2018-07-13 22:26:28 +0300

    Add serializer support. Some improvements

--- src/VuexRailsPlugin.js
+++ src/VuexRailsPlugin.js
@@ -41,120 +41,134 @@ const resourcePath = (resource, params) => {
   }
 }
 
-export default function VuexRailsPlugin(resource) {
+const serialize = (response, serializer) => {
+  if (serializer) {
+    return serializer.parse(response)
+  } else {
+    return response
+  }
+}
+
+export default function VuexRailsPlugin(resource, serializer) {
+  const state = {
+    all: [],
+    current: {},
+    error: null,
+  }
+  
+  const mutations = {
+    [ALL] (state, { items }) {
+      state.error = null
+      state.all = items
+    },
+    [GET] (state, { item }) {
+      state.error = null
+      state.current = item
+      updateItem(state.current, state.all)
+    },
+    [CREATE] (state, { item }) {
+      state.error = null
+      if (state.all.find(i => i.id === item.id) !== undefined) {
+        updateItem(item, state.all)
+      } else {
+        state.all.unshift(item)
+      }
+    },
+    [UPDATE] (state, { item }) {
+      state.error = null
+      state.current = null
+      updateItem(item, state.all)
+    },
+    [DESTROY] (state, { item }) {
+      state.error = null
+      state.current = {}
+      updateItem(item, state.all, true)
+    },
+    [ERROR] (state, { err }) {
+      state.error = err.response && err.response.data ? err.response.data : err
+    }
+  }
+  
+  const actions = {
+    getAll({ commit }, params) {
+      params = params || {}
+      return new Promise((resolve, reject) => {
+        http.get(`/${resource}.json`, { params })
+          .then(res => {
+            const items = serialize(res.data, serializer)
+            commit(ALL, { items })
+            resolve(res)
+          })
+          .catch(err => {
+            commit(ERROR, { err })
+            reject(err)
+          })
+      })
+    },
+    get({ commit }, id, params) {
+      params = params || {}
+      return new Promise((resolve, reject) => {
+        http.get(resourcePath(resource, { id: id }), { params })
+          .then(res => {
+            const item = serialize(res.data, serializer)
+            commit(GET, { item })
+            resolve(res)
+          })
+          .catch(err => {
+            commit(ERROR, { err })
+            reject(err)
+          })
+      })
+    },
+    create({ commit }, item) {
+      return new Promise((resolve, reject) => {
+        http.post(`/${resource}.json`, item)
+          .then(res => {
+            const item = serialize(res.data, serializer)
+            commit(CREATE, { item })
+            resolve(res)
+          })
+          .catch(err => {
+            commit(ERROR, { err })
+            reject(err)
+          })
+      })
+    },
+    update({ commit }, item) {
+      return new Promise((resolve, reject) => {
+        http.put(resourcePath(resource, item), item)
+          .then(res => {
+            const item = serialize(res.data, serializer)
+            commit(UPDATE, { item })
+            resolve(res)
+          })
+          .catch(err => {
+            commit(ERROR, { err })
+            reject(err)
+          })
+      })
+    },
+    destroy({ commit }, item) {
+      return new Promise((resolve, reject) => {
+        http.delete(resourcePath(resource, item))
+          .then(res => {
+            commit(DESTROY, { item })
+            resolve(res)
+          })
+          .catch(err => {
+            commit(ERROR, { err })
+            reject(err)
+          })
+      })
+    },
+  }
+
   return store => {
     store.registerModule(resource, {
       namespaced: true,
-      state: {
-        all: [],
-        current: {},
-        error: null
-      },
-      actions: {
-        getAll({ commit }, params) {
-          params = params || {}
-          return new Promise((resolve, reject) => {
-            http.get(`/${resource}.json`, { params })
-              .then(res => {
-                const items = res.data
-                commit(ALL, { items })
-                resolve(res)
-              })
-              .catch(err => {
-                commit(ERROR, { err })
-                reject(err)
-              })
-          })
-        },
-        get({ commit }, id, params) {
-          params = params || {}
-          return new Promise((resolve, reject) => {
-            http.get(resourcePath(resource, { id: id }), { params })
-              .then(res => {
-                const item = res.data
-                commit(GET, { item })
-                resolve(res)
-              })
-              .catch(err => {
-                commit(ERROR, { err })
-                reject(err)
-              })
-          })
-        },
-        create({ commit }, item) {
-          return new Promise((resolve, reject) => {
-            http.post(`/${resource}.json`, item)
-              .then(res => {
-                const item = res.data
-                commit(CREATE, { item })
-                resolve(res)
-              })
-              .catch(err => {
-                commit(ERROR, { err })
-                reject(err)
-              })
-          })
-        },
-        update({ commit }, item) {
-          return new Promise((resolve, reject) => {
-            http.put(resourcePath(resource, item), item)
-              .then(res => {
-                const item = res.data
-                commit(UPDATE, { item })
-                resolve(res)
-              })
-              .catch(err => {
-                commit(ERROR, { err })
-                reject(err)
-              })
-          })
-        },
-        destroy({ commit }, item) {
-          return new Promise((resolve, reject) => {
-            http.delete(resourcePath(resource, item))
-              .then(res => {
-                commit(DESTROY, { item })
-                resolve(res)
-              })
-              .catch(err => {
-                commit(ERROR, { err })
-                reject(err)
-              })
-          })
-        }
-      },
-      mutations: {
-        [ALL] (state, { items }) {
-          state.error = null
-          state.all = items
-        },
-        [GET] (state, { item }) {
-          state.error = null
-          state.current = item
-          updateItem(state.current, state.all)
-        },
-        [CREATE] (state, { item }) {
-          state.error = null
-          if (state.all.find(i => i.id === item.id) !== undefined) {
-            updateItem(item, state.all)
-          } else {
-            state.all.push(item)
-          }
-        },
-        [UPDATE] (state, { item }) {
-          state.error = null
-          state.current = null
-          updateItem(item, state.all)
-        },
-        [DESTROY] (state, { item }) {
-          state.error = null
-          state.current = null
-          updateItem(item, state.all, true)
-        },
-        [ERROR] (state, { err }) {
-          state.error = err.response && err.response.data ? err.response.data : err
-        }
-      }
+      state:      state,
+      actions:    actions,
+      mutations:  mutations
     })
   }
 }
